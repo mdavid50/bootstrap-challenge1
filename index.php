5cf520ee7b0cc5c06d68feb84214cbae905284c4
mdavid5 <mcdav3636@gmail.com>
2018-02-06 18:48:36 -0700

    example coding

--- index.php
+++ index.php
@@ -14,13 +14,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-
+<section class="nav-sec">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar nav-brand">Title</a>
+    <button class="navbar-toggler text-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <ul class="navbar-nav text-right">
             <li class="nav-item active">
                 <a class="nav-link" href="#!">link 1 </a>
             </li>
@@ -36,9 +37,16 @@
         </ul>
     </div>
 </nav>
+</section>
 
 
-<div>
+<div class="container">
+    <div class="row">
+        <div class="card col-m-3"
+    </div>
+</div>
+
+<div class="footer">
 
 </div>
 </body>
