c360eb9484a6430ec5198724603f77b5b3928a60
mdavid5 <mcdav3636@gmail.com>
2018-02-07 09:37:42 -0700

    start about section

--- index.php
+++ index.php
@@ -15,29 +15,52 @@
 </head>
 <body>
 <section class="nav-sec">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar nav-brand">Title</a>
-    <button class="navbar-toggler text-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <ul class="navbar-nav text-right">
-            <li class="nav-item active">
-                <a class="nav-link" href="#!">link 1 </a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#!">link 2</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#!">link 3</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#!">link 4</a>
-            </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar nav-brand">Title</a>
+        <button class="navbar-toggler text-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav text-right">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#!">link 1 </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#!">link 2</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#!">link 3</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#!">link 4</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="jumbotron">
+        <h1 class="display-3">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.</p>
+        <hr class="my-2">
+        <p>It uses utility classes for typography and
+            spacing to space content out within the larger container.</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#!" role="button">Some action</a>
+        </p>
     </div>
-</nav>
 </section>
+
+<section class="about">
+    <div class="container bg-dark">
+        <div class="row">
+            <div class="col-m-3">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
 
 
 <div class="container">
